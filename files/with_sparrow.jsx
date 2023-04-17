5b54f883deb7e5be9f65c084debb0aa4adb5a915
MiftikCZ <89579269+MiftikCZ@users.noreply.github.com>
2023-04-17 17:15:53 +0200

    first upload

---
+++ files/with_sparrow.jsx
@@ -0,0 +1,62 @@
+<div
+  style={{
+    height: '100%',
+    width: '100%',
+    display: 'flex',
+    textAlign: 'center',
+    alignItems: 'center',
+    justifyContent: 'center',
+    flexDirection: 'column',
+    flexWrap: 'nowrap',
+    backgroundColor: '#18181e',
+    backgroundImage: 'radial-gradient(circle at 25px 25px, #eee5 2%, transparent 1%), radial-gradient(circle at 75px 75px, #eee5 2%, transparent 0%)',
+    backgroundSize: '100px 100px',
+  }}
+>
+  <div
+    style={{
+      display: 'flex',
+      alignItems: 'center',
+      justifyContent: 'center',
+    }}
+  >
+    <div
+      style={{
+        alignItems:"center",
+        display:"flex",
+        backgroundColor:"#eee1",
+        padding:"1em",
+        width:"500px",
+        
+        //lineHeight: 1.8,
+      }}  
+    >
+      <img 
+      height="100%"
+      src="https://cdn-icons-png.flaticon.com/128/3501/3501255.png"
+      height={80}
+      />
+      
+      <div
+    style={{
+      display: 'flex',
+      fontSize: 40,
+      fontStyle: 'normal',
+      textAlign:"center",
+      color: 'white',
+      height:"100%",
+      width:"375",
+
+      justifyContent:"center",
+      middle:"center",
+      fontFamily: "monospace",
+      whiteSpace: 'pre-wrap',
+    }}
+  >
+    <b>Hello there!</b>
+  </div>
+    </div>
+
+  </div>
+  
+</div>
