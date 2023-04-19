d654e3a0f06fa8c4e8f38628da8ae9064efdb018
MiftikCZ <89579269+MiftikCZ@users.noreply.github.com>
2023-04-19 16:42:56 +0200

    Create with_pinguin.jsx

---
+++ files/with_pinguin.jsx
@@ -0,0 +1,67 @@
+<div
+  style={{
+    height: '100%',
+    width: '100%',
+    display: 'flex',
+    textAlign: 'center',
+    alignItems: 'center',
+    justifyContent: 'center',
+    flexDirection: 'column',
+    flexWrap: 'nowrap',
+    backgroundColor: '#181825',
+    backgroundImage: 'radial-gradient(circle at 25px 25px, #eee2 2%, transparent 0%), radial-gradient(circle at 75px 75px, #eee1 2%, transparent 0%)',
+    backgroundSize: '100px 100px',
+  }}
+>
+  <div
+    style={{
+      display: 'flex',
+      alignItems: 'center',
+      justifyContent: 'center',
+    }}
+  >
+    <div
+      style={{
+        alignItems:"center",
+        display:"flex",
+        padding:"1em",
+        width:"500px",
+        //justifyContent:"bottom",
+        flexDirection: "column",
+        //lineHeight: 1.8,
+      }}  
+    >
+      <img 
+      //height="100%"
+      //src="https://cdn-icons-png.flaticon.com/128/523/523461.png"
+      src="https://cdn-icons-png.flaticon.com/128/9296/9296554.png"
+      //src="https://cdn-icons-png.flaticon.com/128/10056/10056529.png"
+      height={80}
+      style={{
+        marginBottom:20
+      }}
+      />
+      
+      <div
+    style={{
+      display: 'flex',
+      fontSize: 40,
+      fontStyle: 'normal',
+      textAlign:"center",
+      color: '#a6e3a1',
+      //height:"100%",
+      width:"375",
+
+      justifyContent:"center",
+      middle:"center",
+      fontFamily: "monospace",
+      whiteSpace: 'pre-wrap',
+    }}
+  >
+    <b>Zdravím!</b>
+  </div>
+    </div>
+
+  </div>
+  
+</div>
